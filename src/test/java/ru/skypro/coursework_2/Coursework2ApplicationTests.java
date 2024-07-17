88c722247c8c0f7a8935e4689e53b8536a8ce24e
darya <darya.kolycheva98@yandex.ru>
2024-07-17 18:25:17 +0300

    Initial commit

---
+++ src/test/java/ru/skypro/coursework_2/Coursework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.coursework_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
